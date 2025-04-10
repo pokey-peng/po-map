27905e74d563c29918bea1a6751f7565787ed5a8
pokey <wr100pokey@gmail.com>
2025-04-10 17:33:55 +0800

    webGL绘图初步

--- src/lib/webGL/gl-help.ts
+++ src/lib/webGL/gl-help.ts
@@ -1,18 +1,26 @@
 const vertexShaderSource = `#version 300 es
-    in vec4 a_position;
+    out vec4 v_color;
+    in vec4 a_color;
+    in vec2 a_position;
+    uniform vec2 u_resolution;
 
     void main() {
-      gl_Position = a_position;
+      vec2 zeroToOne = a_position / u_resolution;
+      vec2 zeroToTwo = zeroToOne * 2.0;
+      vec2 clipSpace = zeroToTwo - 1.0;
+      gl_Position = vec4(clipSpace * vec2(1,-1), 0, 1);
+      v_color = a_color;
     }
 `
 
 const fragmentShaderSource = `#version 300 es
     precision highp float;
-
+    uniform vec4 u_color;
+    in vec4 v_color;
     out vec4 outColor;
 
     void main() {
-        outColor = vec4(1.0, 0.0, 0.5, 1.0);
+        outColor = v_color;
     }
 `
 function createShader(gl: WebGL2RenderingContext, type: GLenum, source: string) {
@@ -41,4 +49,77 @@ export function createProgram(gl: WebGL2RenderingContext) {
   }
   console.error('程序链接失败:', gl.getProgramInfoLog(program))
   gl.deleteProgram(program)
+}
+
+export function resizeCanvasToDisplaySize(canvas: HTMLCanvasElement): boolean {
+  // 获取浏览器显示的画布的CSS像素值
+  const displayWidth = canvas.clientWidth
+  const displayHeight = canvas.clientHeight
+
+  // 检查画布大小是否相同。
+  const needResize = canvas.width !== displayWidth || canvas.height !== displayHeight
+
+  if (needResize) {
+    // 使画布大小相同
+    canvas.width = displayWidth
+    canvas.height = displayHeight
+  }
+
+  return needResize
+}
+
+export function randomInt(range: number): number {
+  // 返回0-range范围内的随机整数
+  return Math.floor(Math.random() * range)
+}
+
+/**
+ *
+ * @param gl webGL上下文
+ * @param program 着色器程序
+ * @param x 矩形左上角x坐标
+ * @param y 矩形左上角坐标
+ * @param width 矩形宽度
+ * @param height 矩形高度
+ * @param color 矩形颜色
+ */
+export function drawRectangle(
+  gl: WebGL2RenderingContext,
+  program: WebGLProgram,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  color: [number, number, number, number],
+) {
+  const x1 = x
+  const y1 = y
+  const x2 = x + width
+  const y2 = y + height
+  const positions = new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2])
+  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW)
+
+  gl.uniform4f(gl.getUniformLocation(program, 'u_color'), color[0], color[1], color[2], color[3])
+
+  const primitiveType = gl.TRIANGLES
+  const offset = 0
+  const count = positions.length / 2
+  gl.drawArrays(primitiveType, offset, count)
+}
+
+export function drawTriangle(
+  gl: WebGL2RenderingContext,
+  program: WebGLProgram,
+  position: number[],
+  color: [number, number, number, number],
+) {
+  const positions = new Float32Array(position)
+  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW)
+
+  gl.uniform4f(gl.getUniformLocation(program, 'u_color'), color[0], color[1], color[2], color[3])
+
+  const primitiveType = gl.TRIANGLES
+  const offset = 0
+  const count = positions.length / 2
+  gl.drawArrays(primitiveType, offset, count)
 }
--- src/views/webgl/webgl-demo.vue
+++ src/views/webgl/webgl-demo.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
-import { createProgram } from '@/lib/webGL/gl-help'
+import {
+  createProgram,
+  resizeCanvasToDisplaySize,
+  drawRectangle,
+  randomInt,
+} from '@/lib/webGL/gl-help'
 const canvas = ref<HTMLCanvasElement | null>(null)
 const gl = shallowRef<WebGL2RenderingContext | null | undefined>(null)
 const init3dGl = () => {
@@ -8,6 +13,7 @@ const init3dGl = () => {
     console.error('WebGL not supported')
     return
   }
+  resizeCanvasToDisplaySize(canvas.value as HTMLCanvasElement)
   // Set the viewport size
   gl.value.viewport(0, 0, canvas.value?.width || 0, canvas.value?.height || 0)
   // Clear the color buffer with specified clear color
@@ -15,19 +21,53 @@ const init3dGl = () => {
   // Clear the color buffer
   gl.value.clear(gl.value.COLOR_BUFFER_BIT)
 
+  // 编译着色器
   const program = createProgram(gl.value) as WebGLProgram
 
+  // 绑定a_position属到缓冲区
   const positionAttrLoc = gl.value.getAttribLocation(program, 'a_position')
   const positionBuffer = gl.value.createBuffer()
-  gl.value.bindBuffer(gl.value.ARRAY_BUFFER, positionBuffer)
 
-  const positions = [0, 0, 0, 0.5, 0.7, 0]
-  gl.value.bufferData(gl.value.ARRAY_BUFFER, new Float32Array(positions), gl.value.STATIC_DRAW)
-
+  // 定义绑定点数组
   const vao = gl.value.createVertexArray()
   gl.value.bindVertexArray(vao)
   gl.value.enableVertexAttribArray(positionAttrLoc)
   gl.value.vertexAttribPointer(positionAttrLoc, 2, gl.value.FLOAT, false, 0, 0)
+
+  gl.value.useProgram(program)
+  gl.value.bindVertexArray(vao)
+
+  const colorAttrLoc = gl.value.getAttribLocation(program, 'a_color')
+  const colorBuffer = gl.value.createBuffer()
+  gl.value.bindBuffer(gl.value.ARRAY_BUFFER, colorBuffer)
+  gl.value.enableVertexAttribArray(colorAttrLoc)
+  gl.value.vertexAttribPointer(colorAttrLoc, 4, gl.value.FLOAT, false, 0, 0)
+
+  // 设置分辨率，转化像素坐标到WebGL坐标
+  const resolutionUniformLoc = gl.value.getUniformLocation(program, 'u_resolution')
+  gl.value.uniform2f(resolutionUniformLoc, canvas.value?.width || 0, canvas.value?.height || 0)
+
+  for (let i = 0; i < 100; i++) {
+    const x = randomInt(canvas.value?.width || 0)
+    const y = randomInt(canvas.value?.height || 0)
+    const width = randomInt(50)
+    const height = randomInt(50)
+    const color1 = [Math.random(), Math.random(), Math.random(), 1]
+    const color2 = [Math.random(), Math.random(), Math.random(), 1]
+    gl.value.bindBuffer(gl.value.ARRAY_BUFFER, colorBuffer)
+    gl.value.bufferData(
+      gl.value.ARRAY_BUFFER,
+      new Float32Array([...color1, ...color1, ...color1, ...color2, ...color2, ...color2]),
+      gl.value.STATIC_DRAW,
+    )
+    gl.value.bindBuffer(gl.value.ARRAY_BUFFER, positionBuffer)
+    drawRectangle(gl.value, program, x, y, width, height, [
+      Math.random(),
+      Math.random(),
+      Math.random(),
+      1,
+    ])
+  }
 }
 onMounted(() => {
   init3dGl()
